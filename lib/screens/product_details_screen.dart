a2890b010bf0a108d25b9895c5aa05155e0cdb96
madhank93 <maddymadhan946@gmail.com>
2021-04-11 15:21:56 +0530

    feat: initial commit of product details screen with provider

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/providers/products_provider.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static final String routeID = "prodcut_details_screen";
 
   @override
   Widget build(BuildContext context) {
+    final productID = ModalRoute.of(context)!.settings.arguments as String;
+
+    final product = Provider.of<ProductsProvider>(
+      context,
+      listen: false,
+    ).findByID(productID);
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
     );
   }
 }
