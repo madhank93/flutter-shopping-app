4660a046d95dde4db6ef1ac54a73a10b1a382b3e
madhank93 <maddymadhan946@gmail.com>
2021-04-13 22:44:05 +0530

    feat: added method remove and calc total amount

--- lib/providers/cart_provide.dart
+++ lib/providers/cart_provide.dart
@@ -14,12 +14,20 @@ class CartProvider with ChangeNotifier {
     return _cartItems.length;
   }
 
+  double get totalAmount {
+    var total = 0.0;
+    _cartItems.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
   void addItem(String productId, double price, String title) {
     if (_cartItems.containsKey(productId)) {
       _cartItems.update(
         productId,
         (value) =>
-            CartItem(value.id, value.title, value.price, value.qunatity + 1),
+            CartItem(value.id, value.title, value.price, value.quantity + 1),
       );
     } else {
       _cartItems.putIfAbsent(
@@ -27,6 +35,11 @@ class CartProvider with ChangeNotifier {
         () => CartItem(DateTime.now().toString(), title, price, 1),
       );
     }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _cartItems.remove(productId);
     notifyListeners();
   }
 }
