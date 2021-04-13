580b31af78364a764c3a7b52b7b6ed2d13f27427
madhank93 <maddymadhan946@gmail.com>
2021-04-13 22:45:04 +0530

    feat: initial commit of product details screen

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -7,16 +7,49 @@ class ProductDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productID = ModalRoute.of(context)!.settings.arguments as String;
-
-    final product = Provider.of<ProductsProvider>(
+    final productId =
+        ModalRoute.of(context)!.settings.arguments as String; // is the id!
+    final loadedProduct = Provider.of<ProductsProvider>(
       context,
       listen: false,
-    ).findByID(productID);
-
+    ).findByID(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(product.title),
+        title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
