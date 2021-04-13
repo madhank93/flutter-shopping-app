4b58ea42c5d17b0f3e9d6ca06470b5d04538bc60
madhank93 <maddymadhan946@gmail.com>
2021-04-13 16:48:38 +0530

    fix: set debug banner to false

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.red,
           accentColor: Colors.grey[350],
